49b7b46e0a34581a1dea8fa77351300aa0b31090
Joseph Schultz <jschultz333@gmail.com>
2022-01-25 20:20:03 -0600

    Create header file with standard html structure

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <header>
+            <?php echo bloginfo('name'); ?>
+        </header>
+    
