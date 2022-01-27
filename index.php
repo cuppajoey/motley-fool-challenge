a0a7806c5f5411486d2b31e3b52b4dfe95ddaa59
Joseph Schultz <jschultz333@gmail.com>
2022-01-26 20:16:36 -0600

    Loads latest posts on landing page

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php get_header(); ?>
 
-<main id="site-content">
-    <p>Hello World</p>
-</main>
+    <main id="site-content">
+
+        <?php if ( have_posts() ) : ?>
+
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <article>
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <p><?php the_excerpt(); ?></p>
+                </article>
+
+            <?php endwhile; ?>
+
+        <?php endif; ?>
+        
+    </main>
 
 <?php get_footer(); ?>
