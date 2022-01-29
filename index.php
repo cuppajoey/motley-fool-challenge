8f36f271edbf941b8baf13875cb89d111d8569db
Joseph Schultz <jschultz333@gmail.com>
2022-01-29 12:04:10 -0600

    Add post meta to home query

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 
                 <article>
                     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <div class="post-meta">
+                        <span class="post-date"><?php echo get_the_date();?></span>
+                        <span class="post-author"><?php echo 'by ' . get_the_author(); ?></span>
+                    </div>
                     <p><?php the_excerpt(); ?></p>
                 </article>
 
