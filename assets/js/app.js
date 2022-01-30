9c4432649fe8c6b08cd0830e5f9d4bcc79531541
Joseph Schultz <jschultz333@gmail.com>
2022-01-30 15:43:26 -0600

    Improves handling of multiple api requests

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,6 +25,50 @@ const getCompanyExchangeData = () => {
         });
 };
 
+const getCompanyStockProfile = () => {
+    let getCompanyQuote = fetch("http://stockadvisor.local/wp-json/mfsa/v1/quote/?symbol=sbux");
+    let getCompanyProfile = fetch("http://stockadvisor.local/wp-json/mfsa/v1/profile/?symbol=sbux");
+    
+    Promise.all([getCompanyQuote, getCompanyProfile])
+        .then(values => Promise.all(values.map(value => value.json())))
+        .then(combinedResponse => {
+            let quoteResponse = combinedResponse[0];
+            let profileResponse = combinedResponse[1];
+
+            console.log(combinedResponse)
+            // updateCompanyStats(quoteResponse, profileResponse);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
+
+const updateCompanyStats = (quoteData, profileData) => {
+    let statsTable = document.getElementById("mfsa-stats");
+    let stats = {
+        "Current Price": quoteData[0].price,
+        "Today's Change": quoteData[0].change,
+        "Change Percentage": quoteData[0].changesPercentage,
+        "Yearly Range:": profileData[0].range,
+        "Beta": profileData[0].beta,
+        "Average Volume": quoteData[0].avgVolume,
+        "Market Cap": quoteData[0].marketCap,
+        "Dividend": profileData[0].lastDiv
+    }
+
+    markup = '<div class="table-grid">';
+        for (let key in stats) {
+            markup += '<div class="table-cell">';
+            markup += '<strong>' + key + '</strong>';
+            markup += '<span>' + stats[key] + '</span>';
+            markup += '</div>';
+        }
+    markup += '</div>';
+
+    statsTable.innerHTML = markup;
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-    getCompanyExchangeData();
+    // getCompanyExchangeData();
+    getCompanyStockProfile();
 })
--- functions.php
+++ functions.php
@@ -148,9 +148,10 @@ register_nav_menus(
  *
  * @since 1.0.0
  */
-function mfsa_get_company_quote($companySymbol) {
+function mfsa_rest_get_company_quote($request) {
     set_time_limit(0);
 
+    $companySymbol = strtoupper($request['symbol']);
     $endpoint = "https://financialmodelingprep.com/api/v3/quote/{$companySymbol}?apikey=e2471ff0e15771593da05997639bf01d";
 
     $channel = curl_init();
@@ -169,10 +170,11 @@ function mfsa_get_company_quote($companySymbol) {
     $output = curl_exec($channel);
 
     if (curl_error($channel)) {
-        return 'error:' . curl_error($channel);
+        return new WP_REST_Response( [
+			'message' => 'Company data not found',
+		], 400 );
     } else {
-        $jsonResults = json_decode($output);
-        return $jsonResults;
+        return new WP_REST_Response( json_decode($output), 200 );
     }
 }
 
@@ -183,9 +185,10 @@ function mfsa_get_company_quote($companySymbol) {
  *
  * @since 1.0.0
  */
-function mfsa_get_company_stats($companySymbol) {
+function mfsa_rest_get_company_profile($request) {
     set_time_limit(0);
 
+    $companySymbol = strtoupper($request['symbol']);
     $endpoint = "https://financialmodelingprep.com/api/v3/profile/{$companySymbol}?apikey=e2471ff0e15771593da05997639bf01d";
 
     $channel = curl_init();
@@ -204,14 +207,16 @@ function mfsa_get_company_stats($companySymbol) {
     $output = curl_exec($channel);
 
     if (curl_error($channel)) {
-        return 'error:' . curl_error($channel);
+        return new WP_REST_Response( [
+			'message' => 'Company data not found',
+		], 400 );
     } else {
-        $jsonResults = json_decode($output);
-        return $jsonResults;
+        return new WP_REST_Response( json_decode($output), 200 );
     }
 }
 
 function mfsa_get_company_callout_box($postID) {
+    return false;
     // Get the symbol assigned to post
     $symbol = get_post_meta($postID, '_mfsa_symbol', true);
     
@@ -219,7 +224,7 @@ function mfsa_get_company_callout_box($postID) {
     if (! $symbol) return false;
     
     // Get company stats from API
-    $keyStats = mfsa_get_company_stats($symbol);
+    $keyStats = mfsa_rest_get_company_profile($symbol);
 
     $logoURL = $keyStats[0]->image;
     $companyName = $keyStats[0]->companyName;
@@ -264,3 +269,55 @@ function mfsa_inject_company_stats_into_post($postContent) {
     return $postContent;
 }
 add_filter( 'the_content', 'mfsa_inject_company_stats_into_post' );
+
+
+/**
+ * Custom Rest API Endpoint that returns a company exchange quote
+ * 
+ * Hooks into rest_api_init action to add a URL endpoint. 
+ * This endpoint calls mfsa_rest_get_company_quote() to return the latest stock quote.
+ * 
+ */
+function mfsa_register_company_quote_endpoint() {
+	
+	// Visit /wp-json/mfsa/v1/quote to get the latest quote
+	register_rest_route( 'mfsa/v1', '/quote', array(
+		'methods' => 'GET',
+		'callback' =>  'mfsa_rest_get_company_quote',
+		'permission_callback' => '__return_true',
+        'args'     => [
+			'symbol' => [
+				'required' => true,
+				'type'     => 'string',
+			],
+		],
+    ) );
+	
+}
+add_action( 'rest_api_init', 'mfsa_register_company_quote_endpoint' );
+
+
+/**
+ * Custom Rest API Endpoint that returns a company exchange profile
+ * 
+ * Hooks into rest_api_init action to add a URL endpoint. 
+ * This endpoint calls mfsa_rest_get_company_profile() to return the latest stock quote.
+ * 
+ */
+function mfsa_register_company_profile_endpoint() {
+	
+	// Visit /wp-json/mfsa/v1/quote to get the latest quote
+	register_rest_route( 'mfsa/v1', '/profile', array(
+		'methods' => 'GET',
+		'callback' =>  'mfsa_rest_get_company_profile',
+		'permission_callback' => '__return_true',
+        'args'     => [
+			'symbol' => [
+				'required' => true,
+				'type'     => 'string',
+			],
+		],
+    ) );
+	
+}
+add_action( 'rest_api_init', 'mfsa_register_company_profile_endpoint' );
